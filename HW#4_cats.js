9b90c669363612e3ebffa503918b37205a47ddbc
MatthewMalefyt <157759162+MatthewMalefyt@users.noreply.github.com>
2024-02-18 17:13:19 -0500

    Create HW#4_cats.js

---
+++ HW#4_cats.js
@@ -0,0 +1,74 @@
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+];
+
+const result = [];
+
+cats.forEach(cat => {
+    if (cat.adoptionStatus === 'available') {
+        result.push(cat.name);
+    }
+});
+
+if (result.length > 0) {
+    const output = `Congratulations! You have adopted ${result.length === 1 ? 'a cat named' : 'cats named'} ${result.join(', ')}.`;
+    console.log(output);
+
+} else {
+    console.log("No cats available for adoption at the moment.");
+}
